74b1ee1d6af4edabe7f673888bb9ef3aa3409331
mbl-35 <math.boyaval@gmail.com>
2021-09-28 09:23:07 +0200

    ci: try powershell (desktop) vs pwsh (server)

--- build.ps1
+++ build.ps1
@@ -54,7 +54,8 @@ task Build {
         powershell -Command "&'Install-Module' -Name ps2exe -Scope CurrentUser"
         #ps2exe .\src\wslctl.ps1 .\build\wslctl.exe
         #Invoke-ps2exe -inputFile .\src\wslctl.ps1 -outputFile .\build\wslctl.exe -nested:$true
-        powershell -Command "&'Invoke-ps2exe' -inputFile .\src\wslctl.ps1 -outputFile .\build\wslctl.exe"
+        Invoke-ps2exe -inputFile .\src\wslctl.ps1 -outputFile .\build\wslctl.exe
+        #powershell -Command "&'Invoke-ps2exe' -inputFile .\src\wslctl.ps1 -outputFile .\build\wslctl.exe"
     }
     catch [Exception] {
         echo $_.Exception|format-list -force
